8862fcc0cf7c9fd0e77139cc37b12721fa3dc16e
Ingy döt Net <ingy@ingy.net>
2014-12-12 14:28:23 -0800

    WIP

--- lib/Dist/Zilla/Plugin/InlineModule.pm
+++ lib/Dist/Zilla/Plugin/InlineModule.pm
@@ -1,6 +1,8 @@
 package Dist::Zilla::Plugin::InlineModule;
 our $VERSION = '0.01';
 
+use XXX;
+
 use Moose;
 extends 'Dist::Zilla::Plugin::MakeMaker::Awesome';
 with 'Dist::Zilla::Role::AfterBuild';
@@ -12,8 +14,24 @@ has module => (
     required => 1,
 );
 
+has stub => (
+    isa => 'ArrayRef[Str]',
+    traits => ['Array'],
+    handles => { stubs => 'elements' },
+    required => 0,
+    default => sub { [] },
+);
+
+has ilsm => (
+    isa => 'ArrayRef[Str]',
+    traits => ['Array'],
+    handles => { ilsms => 'elements' },
+    required => 0,
+    default => sub { ['Inline::C'] },
+);
+
 # Lets us pass the 'module' option more than once:
-sub mvp_multivalue_args { qw(module) }
+sub mvp_multivalue_args { qw(module stub ilsm) }
 
 # Add our FixMakefile call to Makefile.PL, respecting any other footer lines
 # that were provided in dist.ini:
@@ -30,23 +48,23 @@ around _build_footer => sub {
         $self->$orig(@_);
 };
 
+use XXX;
 sub after_build {
     my ($self, $hash) = @_;
+    require Inline::Module;
+
+    my $inline_module = Inline::Module->new(
+        XXX ilsm => $self->ilsm,
+    );
     my $build_dir = $hash->{build_root}->stringify;
     my @inline_modules = map "${_}::Inline", $self->modules;
-    require Inline::Module;
-    local @ARGV = (
+    my @included_modules = $inline_module->included_modules;
+    Inline::Module->handle_distdir(
         $build_dir,
         @inline_modules,
         '--',
-        'Inline',
-        'Inline::denter',
-        'Inline::C',
-        'Inline::C::Parser::RegExp',
-        'Inline::Module',
-        'Inline::Module::MakeMaker',
+        @included_modules,
     );
-    Inline::Module->handle_distdir;
 }
 
 1;
